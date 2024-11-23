c7b2d2b633406b992e79d6eb35560fac053596ca
felipesilvad <felipe.silvad@outlook.com>
2024-11-23 20:08:38 -0300

    a

--- scenes/character.gd
+++ scenes/character.gd
@@ -9,7 +9,7 @@ var is_holding: bool = false
 const THRESHOLDS = [
 	{"hold": "", "action": "short_press_action", "value": 0.1, "color": Color.RED},
 	{"hold": "first_long_hold", "action": "first_long_press_action", "value": 0.75, "color": Color.ORANGE},
-	{"hold": "", "action": "critical_hit_action", "value": 0.85, "color": Color.YELLOW},
+	{"hold": "crit_hold", "action": "critical_hit_action", "value": 0.85, "color": Color.YELLOW},
 	{"hold": "", "action": "second_long_press_action", "value": 1, "color": Color.ORANGE}
 ]
 const MAX_THRESHOLD: float = 1
@@ -17,11 +17,10 @@ const MAX_THRESHOLD: float = 1
 func _process(delta: float) -> void:
 	if hold_time>0:
 		for i in THRESHOLDS.size():
-			if hold_time <= THRESHOLDS[i]["value"] and hold_time >= THRESHOLDS[i-1]["value"]:
-				printt(THRESHOLDS[i]["action"],THRESHOLDS[i]["value"],THRESHOLDS[i-1]["value"])
+			if hold_time <= THRESHOLDS[i]["value"] and hold_time > (THRESHOLDS[i-1]["value"]+0.1):
 				if THRESHOLDS[i]["hold"] != "":
 					call(THRESHOLDS[i]["hold"])
-					return
+					printt(THRESHOLDS[i]["action"],THRESHOLDS[i]["value"],THRESHOLDS[i-1]["value"])
 		
 func evaluate_hold_time() -> void:
 	for threshold in THRESHOLDS:
@@ -37,6 +36,9 @@ func short_press_action() -> void:
 func first_long_hold() -> void:
 	animation_player.play("hold_start")
 	animation_player.queue("hold")
+	
+func crit_hold() -> void:
+	animation_player.play("hold")
 	
 func first_long_press_action() -> void:
 	print("First Long Press detected!")
